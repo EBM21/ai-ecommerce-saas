ab879315c7a84c024272245856a0880636207afb
EBM21 <143330203+EBM21@users.noreply.github.com>
2026-06-02 16:38:17 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
 
   // Define the base domains
   const isLocal = hostname.includes('localhost') || hostname.includes('127.0.0.1')
-  const isVercel = hostname.endsWith('quadlix.com') // <-- Vercel domain ko identify kiya
+  const isVercel = hostname.endsWith('.quadlix.com') // <-- Vercel domain ko identify kiya
   
   const baseDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN || (isLocal ? 'localhost:3000' : 'quadlix.com')
 
